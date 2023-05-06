33f3f5d0813c48f37f19b9d1fa701a3c2364ade9
Samaël Villette <villette.samael@gmail.com>
2023-05-06 12:49:36 +0200

    feat(post): added first entity with test and CI

---
+++ src/Repository/PostRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\PostInterface;
+use Doctrine\Persistence\ObjectRepository;
+
+/**
+ * @extends ObjectRepository<PostInterface>
+ *
+ * @method PostInterface|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PostInterface|null findOneBy(array $criteria, array $orderBy = null)
+ * @method array<int, PostInterface> findAll()
+ * @method array<int, PostInterface> findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+interface PostRepositoryInterface extends ObjectRepository
+{
+
+}
